bccec66981ea5ea4910ca2c8619247b7eab29688
nur <mode0o1208@yahoo.com>
2021-09-10 19:42:27 +0800

    Building React Chart for admin Screen

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Chart } from "../../components/chart/Chart";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
 import { HomePageContainer } from "./homePageSC";
-
+import { chartProps } from "./homPageUtils";
 export const HomePage = () => {
   return (
     <HomePageContainer>
       <FeaturedInfo />
+      <Chart {...chartProps} />
     </HomePageContainer>
   );
 };
---
+++ frontend/src/pages/home/homPageUtils.js
@@ -0,0 +1,8 @@
+import { userData } from "../../dummyData";
+
+export const chartProps = {
+  data: userData,
+  title: "User Analytics",
+  dataKay: "Active User",
+  grid: true,
+};
