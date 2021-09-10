064c165eb66038a2dddc97d66d5b4328f2ec1ba0
nur <mode0o1208@yahoo.com>
2021-09-10 16:17:50 +0800

    Building React sidebar menu component for admin Screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Container, GlobalStyle, OtherContainer } from "./appSC";
+import { Sidebar } from "./components/sidebar/Sidebar";
 import { Topbar } from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Topbar />
+      <Container id="Container">
+        <Sidebar />
+        <OtherContainer>other</OtherContainer>
+      </Container>
     </div>
   );
 }
---
+++ frontend/src/appSC.js
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+a{
+  text-decoration: none;
+}
+
+`;
+export const Container = styled.div`
+  display: flex;
+`;
+
+export const OtherContainer = styled.div`
+  flex: 4;
+  /* background-color: red; */
+`;
