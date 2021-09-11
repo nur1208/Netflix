18e1dc01e010bff1ffdc0f1f37c9702c77a41905
nur <mode0o1208@yahoo.com>
2021-09-11 13:48:03 +0800

    Building React user information component for admin Screen

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Chart } from "../../components/chart/Chart";
 import { FeaturedInfo } from "../../components/featuredInfo/FeaturedInfo";
-import { HomePageContainer } from "./homePageSC";
+import { WidgetLg } from "../../components/widgetLg/WidgetLg";
+import { WidgetSm } from "../../components/widgetSm/WidgetSm";
+import { HomePageContainer, HomeWidget } from "./homePageSC";
 import { chartProps } from "./homPageUtils";
 export const HomePage = () => {
   return (
     <HomePageContainer>
       <FeaturedInfo />
       <Chart {...chartProps} />
+      <HomeWidget>
+        <WidgetSm />
+        <WidgetLg />
+      </HomeWidget>
     </HomePageContainer>
   );
 };
--- frontend/src/pages/home/homePageSC.js
+++ frontend/src/pages/home/homePageSC.js
@@ -3,3 +3,8 @@ import styled from "styled-components";
 export const HomePageContainer = styled.div`
   flex: 4;
 `;
+
+export const HomeWidget = styled.div`
+  display: flex;
+  margin: 20px;
+`;
